d43c0291d6619e5aaea91c967d1fc7eb5b7374fa
Maiz28 <marisol280602@hotmail.com>
2024-04-30 14:04:34 -0600

    "home"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  
+  isScrolled: boolean = false;
+
+
+scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+@HostListener('window:scroll', [])
+onWindowScroll() {
+  if (window.pageYOffset > 100) {
+    this.isScrolled = true;
+  } else {
+    this.isScrolled = false;
+  }
+}
+}
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -14,6 +14,7 @@ export class InicioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
